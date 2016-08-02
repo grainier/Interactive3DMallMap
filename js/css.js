70e2c7e489c05bc49efcafabaf52149eea488f0f
Grainier Perera <grainier@wso2.com>
2016-08-02 20:44:42 +0530

    Adding dynamic floor loading with json & handlebars

--- js/css.js
+++ js/css.js
@@ -2,7 +2,7 @@ var css = css || {};
 css.sheet = null;
 css.media = null;
 
-css.init = function() {
+css.init = function () {
     if (!document.styleSheets) return;
     if (document.getElementsByTagName('head').length == 0) return;
     if (document.styleSheets.length > 0) {
@@ -14,7 +14,7 @@ css.init = function() {
                 if (media === '' || (media.indexOf('screen') !== -1))
                     this.sheet = document.styleSheets[i];
             }
-            else if (this.media=='object') {
+            else if (this.media == 'object') {
                 if (media.mediaText === '' || (media.mediaText.indexOf('screen') !== -1))
                     this.sheet = document.styleSheets[i];
             }
@@ -36,13 +36,13 @@ css.init = function() {
 css.addRule = function (selector, style) {
     if (this.media === 'string') {
         for (var i = 0, l = this.sheet.rules.length; i < l; i++) {
-            if(this.sheet.rules[i].selectorText
-                && this.sheet.rules[i].selectorText.toLowerCase()==selector.toLowerCase()) {
+            if (this.sheet.rules[i].selectorText
+                && this.sheet.rules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
                 this.sheet.rules[i].style.cssText = style;
                 return;
             }
         }
-        this.sheet.addRule(selector,style);
+        this.sheet.addRule(selector, style);
     } else if (this.media === 'object') {
         var styleSheetLength = (this.sheet.cssRules) ? this.sheet.cssRules.length : 0;
         for (var i = 0; i < styleSheetLength; i++) {
@@ -56,23 +56,6 @@ css.addRule = function (selector, style) {
     }
 };
 
-$(function(){
-    css.init()
-    var floors = $(".level");
-    for (var i = 0; i < floors.length; i++) {
-        var l = i + 1;
-        var css_class = "level--" + l;
-        var l_selector = "." + css_class;
-        if (l > 1) {
-            var h = i * (60 / floors.length);
-            css.addRule(l_selector, "-webkit-transform: translateZ(" + h + "vmin); transform: translateZ(" + h + "vmin);");
-            for(var j=i; j > 0; j--) {
-                css.addRule(".levels--selected-"+l+" .level--"+j, "-webkit-transform: translateZ(-60vmin); transform: translateZ(-60vmin);")
-            }
-        }
-        css.addRule(l_selector + "::after", "content: 'L" + l + "';");
-        css.addRule(".levels--selected-" + l + " .level:not(.level--" + l + ")", "opacity: 0;"); /* fade out all others */
-        css.addRule(".levels--selected-" + l, "-webkit-transition-delay: 0.25s; transition-delay: 0.25s;"); /* Delays */
-        $(floors[i]).addClass(css_class);
-    }
+$(function () {
+    css.init();
 });
